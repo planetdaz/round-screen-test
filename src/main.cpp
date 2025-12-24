0c0da00102715fdf943a7805db72760b7031dd48
planetdaz <planetdaz@gmail.com>
2025-12-24 17:44:47 -0600

    Add LED power-on indicator and refactor setup for clarity

--- src/main.cpp
+++ src/main.cpp
@@ -3,22 +3,39 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_GC9A01A.h>
 
-// ---- TFT pin names (as you requested) ----
-#define tft_rst  4   // D2 / GPIO4
-#define tft_cs   5   // D3 / GPIO5
-#define tft_dc   6   // D4 / GPIO6
-#define tft_scl  8   // D8 / GPIO8
-#define tft_sda  10  // D10 / GPIO10
+// ---- LED ----
+#define LED_PIN 2   // D0 / GPIO2 / physical pin 1
+
+// ---- TFT pin names ----
+#define tft_rst  4   // D2 / GPIO4 / pin 3
+#define tft_cs   5   // D3 / GPIO5 / pin 4
+#define tft_dc   6   // D4 / GPIO6 / pin 5
+#define tft_scl  8   // D8 / GPIO8 / pin 9
+#define tft_sda  10  // D10 / GPIO10 / pin 11
 
 Adafruit_GC9A01A tft(tft_cs, tft_dc, tft_rst);
 
+void flashLed(uint8_t times, uint16_t delayMs) {
+  for (uint8_t i = 0; i < times; i++) {
+    digitalWrite(LED_PIN, HIGH);
+    delay(delayMs);
+    digitalWrite(LED_PIN, LOW);
+    delay(delayMs);
+  }
+}
+
 void setup() {
+  // ---- LED power-on indicator ----
+  pinMode(LED_PIN, OUTPUT);
+  flashLed(5, 80);   // rapid 5x blink
+
   Serial.begin(115200);
   delay(200);
 
-  // SPI on ESP32-C3 (SCK, MISO unused, MOSI)
+  // ---- SPI ----
   SPI.begin(tft_scl, -1, tft_sda);
 
+  // ---- TFT ----
   tft.begin();
   tft.fillScreen(GC9A01A_BLACK);
 
@@ -26,7 +43,7 @@ void setup() {
   tft.drawLine(0, 120, 239, 120, GC9A01A_RED);
   tft.drawLine(120, 0, 120, 239, GC9A01A_RED);
 
-  // Concentric circles
+  // Circles
   for (int r = 20; r <= 120; r += 20) {
     tft.drawCircle(120, 120, r, GC9A01A_GREEN);
   }
